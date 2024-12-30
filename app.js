3a106adc4382de1e82f6c715c76e02480cd1c7be
Hardeep Bhatti <hardeepbhatti5656@gmail.com>
2024-12-30 12:41:33 +0530

    2 :- Making the project smoother and functional for all devices.

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+// For Desktops - Mouse Events
+
 let section = document.querySelector('.section_01');
 
-section.addEventListener('mousemove', (event) =>
-{
-    section.style.setProperty('--display', 'block');
+section.addEventListener('mousemove', (event) => {
+
+    section.style.setProperty('--opacity', 1);
 
     let x = (event.offsetX / section.offsetWidth) * 100;
 
     let y = (event.offsetY / section.offsetHeight) * 100;
 
+    section.style.setProperty('--left_position', `${x}%`);
+
+    section.style.setProperty('--top_position', `${y}%`);
+
+    section.style.setProperty('--left', `${x}%`);
+
+    section.style.setProperty('--top', `${y}%`);
+});
+
+section.addEventListener('mouseout', () => {
+
+    section.style.setProperty('--opacity', 0);
+
+    section.style.setProperty('--left', '0%');
+
+    section.style.setProperty('--top', '0%');
+});
+
+
+// For Mobile Devices - Touch Events
+
+function setting_position(x, y) 
+{
     section.style.setProperty('--left_position', x + "%");
 
     section.style.setProperty('--top_position', y + "%");
@@ -15,37 +40,50 @@ section.addEventListener('mousemove', (event) =>
     section.style.setProperty('--left', x + "%");
 
     section.style.setProperty('--top', y + "%");
-});
-
-section.addEventListener('mouseout', () =>
-{
-    section.style.setProperty('--display', 'none');
-
-    section.style.setProperty('--left', '0%');
-
-    section.style.setProperty('--top', '0%');
-});
-
-/* Touch events */
+}
 
 section.addEventListener('touchstart', (event) => {
+
     event.preventDefault();
+
+    section.style.setProperty('--opacity', 1);
+
+    let touch = event.touches[0];
+
+    let touch_x = touch.clientX;
+
+    let touch_y = touch.clientY;
+
+    let rect = section.getBoundingClientRect();
+
+    let x = ((touch_x - rect.left) / section.offsetWidth) * 100;
+
+    let y = ((touch_y - rect.top) / section.offsetHeight) * 100;
+
+    setting_position(x, y);
 });
 
-section.addEventListener('touchmove',(event)=>{
-    section.style.setProperty('--display', 'block');
-    let touch = event.touches[0]; 
-    let x = touch.clientX
-    let y = touch.clientY
+section.addEventListener('touchmove', (event) => {
 
-    section.style.setProperty('--left_position', x + "px");
-    section.style.setProperty('--top_position', y + "px");
-    section.style.setProperty('--left', x + "px");
-    section.style.setProperty('--top', y + "px");
-})
+    event.preventDefault();
 
-section.addEventListener('touchend', () => {
-    section.style.setProperty('--display', 'none');
-    section.style.setProperty('--left', '0px');
-    section.style.setProperty('--top', '0px');
+    let touch = event.touches[0];
+
+    let touch_x = touch.clientX;
+
+    let touch_y = touch.clientY;
+
+    let rect = section.getBoundingClientRect();
+
+    let x = ((touch_x - rect.left) / section.offsetWidth) * 100;
+
+    let y = ((touch_y - rect.top) / section.offsetHeight) * 100;
+
+    setting_position(x, y);
+});
+
+section.addEventListener('touchend', (event) => {
+
+    section.style.setProperty('--opacity', 0);
+
 });
